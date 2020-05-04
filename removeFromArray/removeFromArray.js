53b546cff7e84f5f44bbe41e74906309d1835563
ameliascanlan <ameliascnln@gmail.com>
2020-05-04 12:04:59 +0100

    Added solved exercises to linked repository

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
 const removeFromArray = function() {
+  let noOfArguments = arguments.length;
+  let newArray= arguments[0];
+  let result;
 
+  for (let i=1;i<noOfArguments;i++) {
+    let removeThis= arguments[i];
+    let isItPresent= newArray.indexOf(removeThis);
+
+    console.log(isItPresent);
+
+    if (isItPresent===(-1)) {
+    result = newArray;
+    continue;
+    }
+    else {
+    newArray.splice(newArray.indexOf(removeThis), 1);
+    result = newArray;
+    }
+  }
+
+  return result;
 }
+
 
 module.exports = removeFromArray
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(beginning,end) {
+  let finalSum= 0;
 
+  if (isNaN(beginning)||isNaN(end)||(beginning<0)||(end<0)) {
+    return 'ERROR';
+  }
+
+  else {
+      if (end>=beginning){
+        for (let i=beginning; i<(end+1); i++) {
+          let toAdd=i;
+          finalSum+= toAdd;
+          }
+      }
+
+      else {
+        for (let i=end; i<(beginning+1); i++) {
+          let toAdd=i;
+          finalSum+= toAdd;
+          }
+      }
+
+    return finalSum;
+  }
 }
 
 module.exports = sumAll
