bc9d7f3383ba3c3830415914034231a5e8b1a84b
ameliascanlan <ameliascnln@gmail.com>
2020-05-04 18:16:19 +0100

    Completed temperature conversion task

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+  let input = fahrenheit * 1;
+  let result = ((input-32)/1.8);
+  let finalResult = +result.toFixed(1);
+  return finalResult;
+  }
 
-}
-
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let input = celsius * 1;
+  let result = (input*1.8) + 32;
+  let finalResult = +result.toFixed(1);
+  return finalResult;
 }
 
 module.exports = {
