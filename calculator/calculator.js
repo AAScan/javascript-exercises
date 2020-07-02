421636586465e3bcaab371022dedb29ccb8783c0
ameliascanlan <ameliascnln@gmail.com>
2020-07-02 14:13:14 +0100

    Adding in solutions for Calculator and Palindrome

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,39 @@
-function add () {
-	
+function add (a,b) {
+	const addition= (a*1) + (b*1);
+	return addition;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	const subtraction= (a*1) - (b*1);
+	return subtraction;
 }
 
-function sum () {
-	
+function sum(array) {
+  const operation = array.reduce((total, amount) => total + amount, 0);
+  return operation;
 }
 
-function multiply () {
-	
+function multiply (array) {
+	let baseNumber = 1;
+  for (let i = 0; i < array.length; i++) {
+    let multiplier = array[i];
+    baseNumber *= (multiplier*1)
+	}
+  return baseNumber;
 }
 
-function power() {
-	
+function power(a,b) {
+  let powerResult = Math.pow(a, b);
+  return powerResult;
 }
 
-function factorial() {
-	
+function factorial(number) {
+  let result = 1;
+  for (let i = number; i > 0; i--) {
+    let multiplier = i;
+    result *= multiplier;
+    }
+  return result;
 }
 
 module.exports = {
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,40 @@
-const palindromes = function() {
+const palindromes = function(string) {
+  let lowerCase = string.toLowerCase();
+  let noSpaces = lowerCase.replace(/\s+/g, '');
+  let noPunct = noSpaces.replace(/[.,\/#!$%\^&\*;:{}=\-_`~?()]/g,'');
 
+  let newString = noPunct;
+
+  let stringLength = newString.length;
+
+  if (stringLength % 2 === 0) {
+    let halfway = Math.ceil(stringLength/2);
+    let firstHalf = newString.slice(0,halfway);
+
+    let secondHalf = newString.slice(halfway);
+    let secondHalfArray= secondHalf.split("");
+    let reversedArray= secondHalfArray.reverse();
+    let joinedString= reversedArray.join("");
+
+    if (joinedString === firstHalf) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    let halfway = Math.ceil(stringLength/2);
+    let firstHalf = newString.slice(0,halfway);
+    let secondHalf = newString.slice(halfway-1);
+    let secondHalfArray= secondHalf.split("");
+    let reversedArray= secondHalfArray.reverse();
+    let joinedString= reversedArray.join("");
+
+    if (joinedString === firstHalf) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = palindromes
