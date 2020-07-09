bb8e4d99da606d01f2841fa5f171c60da3715b36
ameliascanlan <ameliascnln@gmail.com>
2020-07-09 14:23:27 +0100

    Added Fibonacci solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n<0){
+    return "OOPS";
+  } else {
+    let startingPoint1=0;
+    let startingPoint2=1;
+    let fibonacciArray= [startingPoint2];
+    for (let i=1; i<=n; i++) {
+      let result = startingPoint1 + startingPoint2;
+     startingPoint1 = startingPoint2;
+      startingPoint2= result;
+      fibonacciArray.push(result);
+    }
+    return fibonacciArray[n-1];
+  }
 }
 
 module.exports = fibonacci
