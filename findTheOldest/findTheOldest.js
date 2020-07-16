9c34088b5c7b0130a7136ac7ee74f1e7a6f0415c
ameliascanlan <ameliascnln@gmail.com>
2020-07-16 16:25:58 +0100

    Added solution to findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-let findTheOldest = function() {
+function getAge(dob,dod) {
+  if (dod === undefined) {
+    let now = new Date();
+    dod = now.getFullYear();
+  }
+  let age = dod - dob;
+  return age;
+}
 
+let findTheOldest = function(array) {
+  return array.reduce(function (currentOldest, currentPerson) {
+    const oldestAge = getAge(currentOldest.yearOfBirth,currentOldest.yearOfDeath);
+    const currentAge = getAge(currentPerson.yearOfBirth,currentPerson.yearOfDeath);
+    if (currentAge > oldestAge) {
+      return currentPerson;
+    }
+    else {
+      return currentOldest;
+    }
+  });
 }
 
 module.exports = findTheOldest
